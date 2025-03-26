c3e0bb686be25c1f5eacfc8d36e2b7b75cc816c4
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2025-03-26 10:37:36 +0100

    update

--- marvel-movie-analysis/marvel_sql/Average IMDB score.sql
+++ marvel-movie-analysis/marvel_sql/Average IMDB score.sql
@@ -1,1 +1,3 @@
+-- Average IMDB --
+
 SELECT AVG(`IMDb (scored out of 10)`) AS AverageIMDB FROM marvel;
--- marvel-movie-analysis/marvel_sql/Directors_Movie_Count.sql
+++ marvel-movie-analysis/marvel_sql/Directors_Movie_Count.sql
@@ -1,3 +1,5 @@
+
+
 SELECT `Director (1)`, COUNT(title) as TotMovie FROM marvel
 GROUP by `Director (1)`
 ORDER BY `TotMovie` DESC;
